ed3d090791d097d4d8d7c9ef379d74906aaab86d
takazumi <takazumikwkm@gmail.com>
2020-02-04 12:23:04 +0900

    2/3時点のコミットです

---
+++ script.js
@@ -0,0 +1,48 @@
+'use strict'
+
+const text = document.getElementById("text");
+
+text.style.color = 'green'
+
+function clickEvent() {
+    alert('クリックされました')
+}
+
+// a tag が押されたときのやつ
+const myfunc1 = () => {
+const menu = document.getElementById("id-menu");
+
+/* ユズノハさんのコード
+    // 現在の開閉状態を判断する
+    if (menu.classList.contains("close")) {
+        // 現在閉じているので開く
+        menu.classList.remove("close");
+    }else {
+        // 現在開いているので閉じる
+        menu.classList.add("close");
+    }
+*/
+
+function hamburger() {
+    　　document.getElementsByClassName('menu').classList.toggle('close');
+    　　document.getElementsByClassName('global-nav').classList.toggle('open');
+    }
+    
+    document.getElementsByClassName('menuWrapper').addEventListener('click', function () {
+    　　hamburger();
+    });
+
+
+};
+
+/*　
+function hamburger() {
+　　document.getElementsByClassName('menu').classList.toggle('close');
+　　document.getElementsByClassName('global-nav').classList.toggle('open');
+}
+
+document.getElementsByClassName('menuWrapper').addEventListener('click', function () {
+　　hamburger();
+});
+
+*/
