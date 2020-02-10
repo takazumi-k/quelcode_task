eb0a5e763a7acb23d5ddd71a95475fb99d567c69
takazumi <takazumikwkm@gmail.com>
2020-02-10 14:55:08 +0900

    PC,SPとも出来上がりつつあります。ご確認お願いいたします。

--- script.js
+++ script.js
@@ -1,47 +1,67 @@
 'use strict'
 
-const text = document.getElementById("text");
+/* メニュータブ */
+const toggleNav = () => {
+  const hamburger = document.getElementById('button');
+  const line = document.getElementById('line');
+  const nav = document.getElementById('sp-nav');
 
-text.style.color = 'green'
-
-function clickEvent() {
-    alert('クリックされました')
+  hamburger.addEventListener('click', function() {
+    line.classList.toggle('close');
+    nav.classList.toggle('open');
+  });
 }
-
-// a tag が押されたときのやつ
-const myfunc1 = () => {
-const menu = document.getElementById("id-menu");
-
-/* ユズノハさんのコード
-    // 現在の開閉状態を判断する
-    if (menu.classList.contains("close")) {
-        // 現在閉じているので開く
-        menu.classList.remove("close");
-    }else {
-        // 現在開いているので閉じる
-        menu.classList.add("close");
-    }
-*/
-
-function hamburger() {
-    　　document.getElementsByClassName('menu').classList.toggle('close');
-    　　document.getElementsByClassName('global-nav').classList.toggle('open');
-    }
-    
-    document.getElementsByClassName('menuWrapper').addEventListener('click', function () {
-    　　hamburger();
-    });
+toggleNav();
 
 
-};
+/* Ｑ＆Ａアコーディオン */
+const qaToggle01 = () => {
+  const arrow= document.getElementById('arrow01');
+  const qa = document.getElementById('qa-close01');
 
-/*　
-function hamburger() {
-　　document.getElementsByClassName('menu').classList.toggle('close');
-　　document.getElementsByClassName('global-nav').classList.toggle('open');
+  arrow.addEventListener('click', function() {
+    qa.classList.toggle('qa-open');
+  });
 }
-document.getElementsByClassName('menuWrapper').addEventListener('click', function () {
-　　hamburger();
-});
+qaToggle01();
 
-*/
+const qaToggle02 = () => {
+  const arrow= document.getElementById('arrow02');
+  const qa = document.getElementById('qa-close02');
+
+  arrow.addEventListener('click', function() {
+    qa.classList.toggle('qa-open');
+  });
+}
+qaToggle02();
+
+const qaToggle03 = () => {
+  const arrow= document.getElementById('arrow03');
+  const qa = document.getElementById('qa-close03');
+
+  arrow.addEventListener('click', function() {
+    qa.classList.toggle('qa-open');
+  });
+}
+qaToggle03();
+
+const qaToggle04 = () => {
+  const arrow= document.getElementById('arrow04');
+  const qa = document.getElementById('qa-close04');
+
+  arrow.addEventListener('click', function() {
+    qa.classList.toggle('qa-open');
+  });
+}
+qaToggle04();
+
+const qaToggle05 = () => {
+  const arrow= document.getElementById('arrow05');
+  const qa = document.getElementById('qa-close05');
+
+  arrow.addEventListener('click', function() {
+    qa.classList.toggle('qa-open');
+  });
+}
+qaToggle05();
+/* Ｑ＆Ａアコーディオン ここまで */
